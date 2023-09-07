1e82485edc229e4bc34e90dea4e7a97b9dad86d6
numerical43 <numerical43@daum.net>
2023-09-07 11:04:58 +0900

    setting : 초기 설정

---
+++ src/main/java/com/isthisteamisthis/umchiumtee/UmchiumteeApplication.java
@@ -0,0 +1,13 @@
+package com.isthisteamisthis.umchiumtee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmchiumteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmchiumteeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isthisteamisthis/umchiumtee/UmchiumteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isthisteamisthis.umchiumtee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmchiumteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
