332c1d519db4e265acb2d1af830011bc8219beed
Son Jung In <88484476+Dylan-SonJungin@users.noreply.github.com>
2023-09-11 17:12:06 +0900

    swagger dependency 수정

--- src/main/java/com/isthisteamisthis/umchiumtee/user/command/application/controller/UserCommandController.java
+++ src/main/java/com/isthisteamisthis/umchiumtee/user/command/application/controller/UserCommandController.java
@@ -1,9 +1,9 @@
 package com.isthisteamisthis.umchiumtee.user.command.application.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 @Tag(name = "회원 Command API")
 @RestController
 @RequiredArgsConstructor
