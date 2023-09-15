38dc9c5912c5a456c60af65f48f9c808b38c433a
Son Jung In <88484476+Dylan-SonJungin@users.noreply.github.com>
2023-09-15 20:39:18 +0900

    추천 음악 entity, data.sql

---
+++ src/main/java/com/isthisteamisthis/lalalia/rangesongdata/query/domain/aggregate/entity/RangeSongData.java
@@ -0,0 +1,24 @@
+package com.isthisteamisthis.lalalia.rangesongdata.query.domain.aggregate.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Table(name = "TBL_RANGE_SONG_DATA")
+public class RangeSongData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long songDataNo;
+
+    @Column
+    private String songName;
+
+    @Column
+    private String artistName;
+}
--- src/main/resources/db/data/data.sql
+++ src/main/resources/db/data/data.sql
@@ -1,2 +1,51 @@
 INSERT INTO TBL_USER(user_no, user_id, user_intro, email, category)
 VALUES (1, 1234123530, '가수가 되고 싶은 학생입니다!', 'singer@gmail.com', 'SINGER');
+
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (1, '모든날모든순간','폴 킴');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (2, '나에게그대만이','유해준');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (3, '내가아니라도','주호');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (4, '너의모든순간','성시경');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (5, '다시만날수있을까','임영웅');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (6, '달빛에그려지는','조미연');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (7, '모래알갱이','임영웅');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (8, '물론','지아');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (9, '사건의지평선','윤하');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, cover_image_file, song_name, artist_name)
+VALUES (10, '사랑그게뭔데','양파');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (11, '사랑은늘도망가','이문세');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (12, '사랑의바보','더 넛츷');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (13, '사랑인가봐','멜로망스');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (14, '사랑하지않아서그랬어','임한별');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (15, '사막에서꽃을피우듯','우디');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (16, '사실말야내가말야그게그러니까말이야','케이시');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (17, '우리들의블루스','임영웅');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (18, '잘지내자우리','로이킴');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (19, '취중고백','김민석');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (20, '헤어지자말해요','박재정');
+INSERT INTO TBL_RANGE_SONG_DATA(data_song_no, song_name, artist_name)
+VALUES (21, '희재','성시경');
+
+
+
+
+
+
